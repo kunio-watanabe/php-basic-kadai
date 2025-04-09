6db2d76950b05e06d375187eb291a170558263fb
kunio.watanabe <0712yumika@gmail.com>
2025-04-09 18:14:33 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,21 @@
+<p>
+    <?php
+
+    $personal_datas = [
+
+        '名前' => '玉ねぎ',
+        '値段' => 200,
+        '産地' => '北海道'
+    ];
+
+    foreach ($personal_datas as $key => $value) {
+
+    echo "{$key}:{$value}<br>";
+    }
+    ?>
+
+<p>
+
+
+
+
